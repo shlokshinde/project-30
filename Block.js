53d1e0dfb268c07f3e5d9f7978c9f81b8314686a
shlokshinde <75329415+shlokshinde@users.noreply.github.com>
2021-04-05 21:17:48 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,31 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visibility = 255;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      if (this.body.speed < 4.5) {
+        var pos= this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width, this.height);
+      }
+      else{
+        World.remove(world,this.body);
+        push()
+        this.visibility = this.visibility-5;
+        tint(255,this.visibility);
+        image(this.image,this.body.position.x,this.body.position.y,50,50);
+        pop()
+      }
+    }
+    }
